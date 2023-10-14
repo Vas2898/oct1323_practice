a4c9aa54bf347f19f362c7a9e076dd1a4095141b
vasanthakumar <109866822+Vas2898@users.noreply.github.com>
2023-10-14 07:36:49 +0530

    Update math_roundoff.

--- math_roundoff.java
+++ math_roundoff.java
@@ -2,11 +2,11 @@ public class Mathdemo {
 
 	public static void main(String[] args) {
 		Mathdemo obj = new Mathdemo();
-		obj.find_floor(100.8);
+		obj.find_roundoff(100.8);
 		
 	}
 
-	private void find_floor(double no) {
+	private void find_roundoff(double no) {
 		double div = no - (int)no;
 		//System.out.println(div);
 		if (div < .5) {
